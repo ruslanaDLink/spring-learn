f19706960cb2533f45f1d26f38e45eae1ae83164
Ruslana Babiak <ruslana.babiak@gmail.com>
2023-12-07 16:15:49 +0100

    Added all files from a new application.

---
+++ src/main/java/pl/babiak/ruslana/spring/learn/SpringLearnApplication.java
@@ -0,0 +1,17 @@
+package pl.babiak.ruslana.spring.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearnApplication.class, args);
+	}
+
+}
+// TODO: 04/12/2023
+// stworzyc nowa oddzielna aplikcje spring boot + spring framework za pomoca https://start.spring.io/
+// umiescic aplikacke spring-learn na git
+// zapoznac sie z artykulem https://geek.justjoin.it/komunikacja-frontend-www-z-backend-w-javie/
---
+++ src/test/java/pl/babiak/ruslana/spring/learn/SpringLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.babiak.ruslana.spring.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
