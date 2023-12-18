a210ae43f6d1878393171d0894ba808bf7da671d
Ruslana Babiak <ruslana.babiak@gmail.com>
2023-12-18 11:47:41 +0100

    Added DID with NoteService.

--- src/main/java/pl/babiak/ruslana/spring/learn/web/NoteController.java
+++ src/main/java/pl/babiak/ruslana/spring/learn/web/NoteController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.babiak.ruslana.spring.learn.Service.NoteService;
 
 import java.util.logging.Logger;
 
@@ -13,6 +14,12 @@ import java.util.logging.Logger;
 public class NoteController {
     private static final Logger LOGGER = Logger.getLogger(NoteController.class.getName());
 
+    private NoteService noteService;
+
+    public NoteController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
     public void create() {
 
     }
@@ -20,7 +27,7 @@ public class NoteController {
     @GetMapping
     public String read(String age) {
         LOGGER.info("read(" + age + ")");
-
+        noteService.read();
         LOGGER.info("read()=");
         return "note-read.html";
     }
