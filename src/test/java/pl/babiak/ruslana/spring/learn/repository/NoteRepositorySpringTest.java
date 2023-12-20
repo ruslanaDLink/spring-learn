29b1eb4189474b7f614c5d9d265d52123bc4e27d
Ruslana Babiak <ruslana.babiak@gmail.com>
2023-12-20 13:21:10 +0100

    Code cleanup.

--- src/test/java/pl/babiak/ruslana/spring/learn/repository/NoteRepositorySpringTest.java
+++ src/test/java/pl/babiak/ruslana/spring/learn/repository/NoteRepositorySpringTest.java
@@ -1,8 +1,10 @@
 package pl.babiak.ruslana.spring.learn.repository;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import pl.babiak.ruslana.spring.learn.repository.entity.NoteEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,6 +15,16 @@ class NoteRepositorySpringTest {
 
     @Test
     void create() {
+        //given
+        NoteEntity noteEntity = new NoteEntity();
+        noteEntity.setTitle("Random Title");
+        noteEntity.setContent("Random Content");
+
+        //when
+        NoteEntity savedNoteEntity = noteRepository.save(noteEntity);
+
+        //then
+        Assertions.assertNotNull(savedNoteEntity, "Saved Note Entity is null.");
     }
 
     @Test
