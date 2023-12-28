bef07ec3a4e326bd1400c1b10c7a9dae1dbf5f4e
Ruslana Babiak <ruslana.babiak@gmail.com>
2023-12-28 13:52:07 +0100

    Added repository annotation.

--- src/main/java/pl/babiak/ruslana/spring/learn/repository/AuthorRepository.java
+++ src/main/java/pl/babiak/ruslana/spring/learn/repository/AuthorRepository.java
@@ -1,8 +1,10 @@
 package pl.babiak.ruslana.spring.learn.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.babiak.ruslana.spring.learn.repository.entity.AuthorEntity;
 
+@Repository
 public interface AuthorRepository extends JpaRepository<AuthorEntity, Long> {
 
 }
